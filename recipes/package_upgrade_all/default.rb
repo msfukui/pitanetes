6aa31f8c499c66585d4daab1a57e81a02d72130e
msfukui <msfukui@gmail.com>
2021-02-11 11:11:26 +0900

    Mainte package_upgrade_all recipe

--- recipes/package_upgrade_all/default.rb
+++ recipes/package_upgrade_all/default.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 execute 'package upgrade all' do
-  command 'sudo apt update && sudo apt upgrade --yes'
+  command 'sudo apt update -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt autoclean'
 end
