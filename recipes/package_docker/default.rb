9c6e2b9cf76f912cbfbfb57e1988de4f2501d954
msfukui <msfukui@gmail.com>
2021-02-11 12:01:10 +0900

    Add Docker package install recipe

---
+++ recipes/package_docker/default.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+# Uninstall old versions docker
+%w[
+  docker
+  docker-engine
+  docker.io
+  containerd
+  runc
+].each do |p|
+  package p do
+    action :remove
+  end
+end
+
+# Install using the repository for Docker
+%w[
+  apt-transport-https
+  ca-certificates
+  curl
+  gnupg-agent
+  software-properties-common
+].each do |p|
+  package p do
+    action :install
+  end
+end
+
+execute 'Add Docker\'s official GPG key' do
+  command 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
+end
+
+execute 'Set up the stable repository for Docker' do
+  command \
+    'sudo add-apt-repository ' \
+    '"deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
+end
+
+execute 'Update apt resources' do
+  command 'sudo apt update -y'
+end
+
+# Install Docker Engine
+%w[
+  docker-ce
+  docker-ce-cli
+  containerd.io
+].each do |p|
+  package p do
+    action :install
+  end
+end
