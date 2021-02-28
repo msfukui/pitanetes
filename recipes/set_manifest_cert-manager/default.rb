51dcead1ca8a2bc2f9e5d6f42adc1c78ae3eb47a
msfukui <msfukui@gmail.com>
2021-02-28 16:42:08 +0900

    Add a recipe to apply cert-manager manifest for master node

---
+++ recipes/set_manifest_cert-manager/default.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# c.f. https://cert-manager.io/docs/installation/kubernetes/
+
+user = node[:host][:user]
+
+# cert-manager's manifest URL
+cert_manager_url = 'https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml'
+
+execute 'Installration: kubectl apply -f cert-manager_url' do
+  command "export KUBECONFIG=/home/#{user}/.kube/config && " \
+    "kubectl apply -f #{cert_manager_url}"
+  not_if  "export KUBECONFIG=/home/#{user}/.kube/config && " \
+    "kubectl get pods --namespace cert-manager | grep 'cert-manager'"
+end
--- roles/master.rb
+++ roles/master.rb
@@ -3,4 +3,5 @@
 include_recipe '../recipes/set_kubeadm_for_master'
 include_recipe '../recipes/set_manifest_flannel'
 include_recipe '../recipes/set_manifest_metallb'
+include_recipe '../recipes/set_manifest_cert-manager'
 # include_recipe '../recipes/package_ddclient/default.rb'
