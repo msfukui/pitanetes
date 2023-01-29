67dd7dfedf2ec21b9a976fdcc593f146716f8546
msfukui <msfukui@gmail.com>
2023-01-29 19:57:15 +0900

    Upgrade ingress-nginx-controller from v0.44.0 to v1.5.1

--- recipes/set_manifest_ingress-nginx/default.rb
+++ recipes/set_manifest_ingress-nginx/default.rb
@@ -4,32 +4,13 @@
 #      https://kubernetes.github.io/ingress-nginx/deploy/baremetal/
 #
 # ingress-nginx's manifest URL
-# https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.44.0/deploy/static/provider/baremetal/deploy.yaml
+# https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml
 
 user = node[:host][:user]
-loadbalancer_ip = node[:ingress_nginx][:loadbalancer_ip]
 
-directory "/home/#{user}/manifests/pitanetes/ingress-nginx" do
-  action :create
-  path "/home/#{user}/manifests/pitanetes/ingress-nginx"
-  mode '750'
-  owner user
-  group user
-end
-
-template "/home/#{user}/manifests/pitanetes/ingress-nginx/deploy.yaml" do
-  action :create
-  path "/home/#{user}/manifests/pitanetes/ingress-nginx/deploy.yaml"
-  source './templates/manifests/pitanetes/ingress-nginx/deploy.yaml.erb'
-  mode '640'
-  owner user
-  group user
-  variables(loadbalancer_ip: loadbalancer_ip)
-end
+deploy_url = 'https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/cloud/deploy.yaml'
 
 execute 'Installration: kubectl apply -f deploy.yaml' do
   command "export KUBECONFIG=/home/#{user}/.kube/config && " \
-    "kubectl apply -f /home/#{user}/manifests/pitanetes/ingress-nginx/deploy.yaml"
-  subscribes :run, "template[/home/#{user}/manifests/pitanetes/ingress-nginx/deploy.yaml]"
-  action :nothing
+    "kubectl apply -f #{deploy_url}"
 end
